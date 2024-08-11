49e0b68496526a25ca97e346a2d90105fed826e7
paulaji <paulajiparayil123@gmail.com>
2024-08-12 00:36:48 +0530

    color

--- soreloser/app/index.jsx
+++ soreloser/app/index.jsx
@@ -1,23 +1,62 @@
-import { StyleSheet, Image, Platform, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  Platform,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 
 export default function SoreLoser() {
   return (
     <View style={styles.titleContainer}>
-      <Text style={styles.welcomeText}>Sore LOSER!</Text>
+      <Text>
+        <Text style={{ color: 'white', fontWeight: '100', fontSize: 50 }}>
+          Sore
+        </Text>
+        <Text
+          style={{
+            color: 'red',
+            fontWeight: '900',
+            fontSize: 50,
+            fontStyle: 'italic',
+          }}
+        >
+          LOSER
+        </Text>
+      </Text>
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'white',
+          paddingHorizontal: 10,
+          paddingVertical: 10,
+          borderRadius: 5,
+        }}
+      >
+        <Text style={{ color: 'black', fontSize: 20 }}>
+          I am ready to lose!{' '}
+          <Text
+            style={{ color: 'red', fontStyle: 'italic', fontWeight: 'bold' }}
+          >
+            Let's Go
+          </Text>
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
+    flexDirection: 'col',
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
+    backgroundColor: 'black',
   },
   welcomeText: {
-    color: 'green',
-    fontSize: 22,
-    fontWeight: 'bold',
+    color: 'red',
+    fontSize: 30,
+    fontWeight: '300',
   },
 });
